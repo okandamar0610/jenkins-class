2aced2bcb8a324252a173e31cce1f75c3e9b869f
Okan Damar <okandamar@Okans-MacBook-Pro-6.local>
2020-09-06 00:54:53 -0700

    kubernetes with jenkins

---
+++ all-packages.groovy
@@ -0,0 +1,58 @@
+// Uniq name for the pod or slave 
+def k8slabel = "jenkins-pipeline-${UUID.randomUUID().toString()}"
+def slavePodTemplate = """
+      metadata:
+        labels:
+          k8s-label: ${k8slabel}
+        annotations:
+          jenkinsjoblabel: ${env.JOB_NAME}-${env.BUILD_NUMBER}
+      spec:
+        affinity:
+          podAntiAffinity:
+            requiredDuringSchedulingIgnoredDuringExecution:
+            - labelSelector:
+                matchExpressions:
+                - key: component
+                  operator: In
+                  values:
+                  - jenkins-jenkins-master
+              topologyKey: "kubernetes.io/hostname"
+        containers:
+        - name: terraform
+          image: hashicorp/terraform:0.12.27
+          imagePullPolicy: IfNotPresent
+          command:
+          - cat
+          tty: true
+        - name: docker
+          image: docker:latest
+          imagePullPolicy: IfNotPresent
+          command:
+          - cat
+          tty: true
+          volumeMounts:
+            - mountPath: /var/run/docker.sock
+              name: docker-sock
+        serviceAccountName: default
+        securityContext:
+          runAsUser: 0
+          fsGroup: 0
+        volumes:
+          - name: docker-sock
+            hostPath:
+              path: /var/run/docker.sock
+    """
+podTemplate(name: k8slabel, label: k8slabel, yaml: slavePodTemplate, showRawYaml: true) {
+    node(k8slabel) {
+    stage("Docker check") {
+        container("docker") {
+            sh 'docker --version'
+        }
+    }
+    stage("Terraform Check") {
+        container("terraform") {
+            sh 'terraform version'
+        }
+    }
+    }
+}
