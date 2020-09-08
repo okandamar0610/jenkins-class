b4d8b465adf55855eba759a99cfb8b9a8735a183
Okan Damar <okandamar@Okans-MacBook-Pro-6.local>
2020-09-08 16:46:21 -0700

    Jenkin Builder added

---
+++ Jenkinsbuilder.groovy
@@ -0,0 +1,53 @@
+// Uniq name for the pod or slave 
+def k8slabel = "jenkins-pipeline-${UUID.randomUUID().toString()}"
+// yaml def for slaves 
+def slavePodTemplate = """
+      metadata:
+        labels:
+          k8s-label: ${k8slabel}
+        annotations:
+          jenkinsjoblabel: ${env.JOB_NAME}-${env.BUILD_NUMBER}
+      spec:
+        affinity:
+          podAntiAffinity:
+            requiredDuringSchedulingIgnoredDuringExecution:
+            - labelSelector:
+                matchExpressions:
+                - key: component
+                  operator: In
+                  values:
+                  - jenkins-jenkins-master
+              topologyKey: "kubernetes.io/hostname"
+        containers:
+        - name: docker
+          image: docker:latest
+          imagePullPolicy: IfNotPresent
+          command:
+          - cat
+          tty: true
+          volumeMounts:
+            - mountPath: /var/run/docker.sock
+              name: docker-sock
+        serviceAccountName: default
+        securityContext:
+          runAsUser: 0
+          fsGroup: 0
+        volumes:
+          - name: docker-sock
+            hostPath:
+              path: /var/run/docker.sock
+    """
+    podTemplate(name: k8slabel, label: k8slabel, yaml: slavePodTemplate, showRawYaml: false) {
+      node(k8slabel) {
+
+          stage("Checkout SCM"){
+              checkout scm 
+          }
+          dir('deployments/docker') {
+              stage("checking"){
+                  sh 'ls -l'
+              }
+          }
+          
+      }
+    }
