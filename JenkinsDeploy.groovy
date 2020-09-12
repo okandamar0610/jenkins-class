497514e92ef4a53df1a0c6f8feb69d59a34bb6a8
Okan Damar <okandamar@Okans-MacBook-Pro-7.local>
2020-09-12 14:11:01 -0700

    added stages to apply job

--- JenkinsDeploy.groovy
+++ JenkinsDeploy.groovy
@@ -1,3 +1,7 @@
+// Uniq name for the pod or slave 
+def k8slabel = "jenkins-pipeline-${UUID.randomUUID().toString()}"
+
+
 properties([
     [$class: 'RebuildSettings', autoRebuild: false, rebuildDisabled: false], 
     parameters([
@@ -7,6 +11,57 @@ properties([
         choice(choices: ['dev', 'qa', 'stage', 'prod'], description: 'Please provide the environment to deploy ', name: 'environment')
         ])
     ])
+
+def slavePodTemplate = """
+      metadata:
+        labels:
+          k8s-label: ${k8slabel}
+        annotations:
+          jenkinsjoblabel: ${env.JOB_NAME}-${env.BUILD_NUMBER}
+      spec:
+        affinity:
+          podAntiAffinity:
+            requiredDuringSchedulingIgnoredDuringExecution:
+            - labelSelector:
+                matchExpressions:
+                - key: component
+                  operator: In
+                  values:
+                  - jenkins-jenkins-master
+              topologyKey: "kubernetes.io/hostname"
+        containers:
+        - name: fuchicorptools
+          image: fuchicorp/buildtools
+          imagePullPolicy: Always
+          command:
+          - cat
+          tty: true
+        serviceAccountName: default
+        securityContext:
+          runAsUser: 0
+          fsGroup: 0
+        volumes:
+          - name: docker-sock
+            hostPath:
+              path: /var/run/docker.sock
+    """
+    podTemplate(name: k8slabel, label: k8slabel, yaml: slavePodTemplate, showRawYaml: false) {
+      node(k8slabel) {
+
+        stage("Checkout SCM") {
+            git 'https://github.com/okandamar0610/jenkins-class.git'
+
+        }  
+
+
+        stage("Apply/Plan") {
+            container("fuchicorptools") {
+                sh 'kubectl version'
+            }
+        }
+        
+      }
+    }
 
 
 println(
