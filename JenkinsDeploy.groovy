26a4e332b5f9f385e26cb651c3db2ca9be2105d9
Okan Damar <okandamar@Okans-MacBook-Pro-7.local>
2020-09-12 18:40:08 -0700

    updated

--- JenkinsDeploy.groovy
+++ JenkinsDeploy.groovy
@@ -58,7 +58,7 @@ def slavePodTemplate = """
                         if (!params.destroyChanges) {
                             if (params.applyChanges) {
                                 println("Applying the changes!")
-                                 sh "sed 's/latest/${params.selectedDockerImage}/' deploy.yaml"
+                                 sh 'sed "s/latest/${params.selectedDockerImage}/" deploy.yaml'
                                  sh 'kubectl apply -f deploy.yaml'
                             } else {
                                 println("Planing the changes")
